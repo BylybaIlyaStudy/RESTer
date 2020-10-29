19cfec3b21c1fa764577370a4d4efe8a470194a0
BylybaIlyaStudy <73159350+BylybaIlyaStudy@users.noreply.github.com>
2020-10-29 12:15:03 +0700

    Add files via upload

---
+++ Poster/Poster/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Poster
+{
+    class Program
+    {
+        static void Main()
+        {
+            PostRequestAsync();
+        }
+
+        private static void PostRequestAsync()
+        {
+            string method = "";
+
+            while (method != "e")
+            {
+                Console.Clear();
+                Console.WriteLine("p - post");
+                Console.WriteLine("u - put");
+                Console.WriteLine("d - delete");
+                Console.WriteLine("e - exit");
+
+                method = Console.ReadLine().ToLower();
+
+                WebRequest request = WebRequest.Create("https://localhost:5001/api/userstatistics");
+
+                switch (method)
+                {
+                    case "p":
+                        request.Method = "POST";
+                        break;
+                    case "u":
+                        request.Method = "PUT";
+                        break;
+                    case "d":
+                        request.Method = "DELETE";
+                        break;
+                    case "e":
+                        Console.WriteLine("выход...");
+                        break;
+                    default:
+                        Console.WriteLine("ошибка");
+                        break;
+                }
+
+                if (method == "e") break;
+
+                Console.Write("username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("version: ");
+                string version = Console.ReadLine();
+
+                Console.Write("os: ");
+                string os = Console.ReadLine();
+
+                if (username == "") username = "default";
+                if (version == "") version = "n/a";
+                if (os == "") os = "n/a";
+
+                Console.WriteLine($"user: {username}, {version}, {os}");
+
+                UserStatistics user = new UserStatistics(username, DateTime.Now, version, os);
+
+                string data = JsonSerializer.Serialize<UserStatistics>(user);
+
+                Console.WriteLine(data);
+
+                // преобразуем данные в массив байтов
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+                // устанавливаем тип содержимого - параметр ContentType
+                request.ContentType = "application/json";
+                // Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+                request.ContentLength = byteArray.Length;
+
+                //записываем данные в поток запроса
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+
+                WebResponse response = request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using StreamReader reader = new StreamReader(stream);
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+                response.Close();
+                Console.WriteLine("Запрос выполнен...");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Poster/Poster/UserStatistics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Poster
+{
+    public class UserStatistics
+    {
+        public string NameOfNode { get; set; }
+        public string DateTimeOfLastStatistics { get; set; }
+        public string VersionOfClient { get; set; }
+        public string TypeOfDevice { get; set; }
+
+        public UserStatistics(string nameOfNode, DateTime dateTime, string versionOfClient, string typeOfDevice)
+        {
+            this.NameOfNode = nameOfNode;
+            this.DateTimeOfLastStatistics = dateTime.ToString();
+            this.VersionOfClient = versionOfClient;
+            this.TypeOfDevice = typeOfDevice;
+        }
+
+        public UserStatistics(string nameOfNode, string versionOfClient, string typeOfDevice) : 
+            this(nameOfNode, DateTime.Now, versionOfClient, typeOfDevice) { }
+
+        public UserStatistics() { }
+    }
+}
